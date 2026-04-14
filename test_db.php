70853b25d448ff72af71939cc3fa5ffb69b2e184
eldoradopablo <eldoradopabloreal@gmail.com>
2026-04-14 21:20:18 +0700

    chore: setup .gitignore and remove sensitive files from tracking

--- test_db.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('ACCESS', true);
-require 'config/db.php';
-file_put_contents('test_db.json', json_encode([
-    'calon' => $pdo->query('EXPLAIN calon_mhs')->fetchAll(PDO::FETCH_ASSOC),
-    'mhs' => $pdo->query('EXPLAIN mahasiswa')->fetchAll(PDO::FETCH_ASSOC)
-], JSON_PRETTY_PRINT));
--- vendor/phpqrcode/qrlib.php
+++
@@ -1,4 +0,0 @@
-<?php
-// vendor/phpqrcode/qrlib.php
-// This is a placeholder for the phpqrcode library. Please download the official phpqrcode library from https://sourceforge.net/projects/phpqrcode/ and place it in this directory.
-// Usage: require_once '../../vendor/phpqrcode/qrlib.php';
